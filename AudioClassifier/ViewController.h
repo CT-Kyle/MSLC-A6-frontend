860094814396165f8ff92d17d359d2573c4134fb
Luke Oglesbee <lukewoglesbee@gmail.com>
2016-11-10 13:54:32 -0600

    Fresh xcode project

---
+++ AudioClassifier/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioClassifier
+//
+//  Created by Luke Oglesbee on 11/10/16.
+//  Copyright © 2016 LukeOglesbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AudioClassifier/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AudioClassifier
+//
+//  Created by Luke Oglesbee on 11/10/16.
+//  Copyright © 2016 LukeOglesbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
